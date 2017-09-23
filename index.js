3148c67422668fed29af89b9f8615f6c5e93c206
apocalypse575 <apocalypse575@gmail.com>
2017-09-23 18:38:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(STRING) {
+function sayHiToGrandma(string) {
   if (string = string.toLowerCase) {
     return "I can't hear you!"
   }
