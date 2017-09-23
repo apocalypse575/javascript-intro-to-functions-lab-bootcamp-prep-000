e536b4daa763ce9c88531c49206b34516cb665cf
apocalypse575 <apocalypse575@gmail.com>
2017-09-23 18:31:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ function sayHiToGrandma(string) {
   if (string = string.toLowerCase) {
     return "I can't hear you!"
   }
-  else if ('string' = string.toUpperCase) {
+  else if (string = string.toUpperCase) {
     return "YES INDEED!"
     }
+  else (string = string.toUpperCase) {
+    return "YES INDEED!"
+      }
 }
